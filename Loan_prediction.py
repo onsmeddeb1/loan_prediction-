98f44cc37bf4a695678e0f140ab4ab3dd416a712
onsmeddeb1 <ons.meddeb.om@gmail.com>
2025-03-17 23:35:13 +0100

    loan prediction

---
+++ Loan_prediction.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import numpy as np
+
+
+with open('Loan_prediction_dataset.csv', mode='r') as file:
+
+    data = np.genfromtxt(file, delimiter=',', skip_header=1, usecols=(8), filling_values=np.nan)
+
+
+    loan_amounts = data
+
+
+    mean_loan_amount = np.nanmean(loan_amounts)
+    median_loan_amount = np.nanmedian(loan_amounts)
+    std_loan_amount = np.nanstd(loan_amounts)
+
+
+    print(f"Mean Loan Amount: {mean_loan_amount:.2f}")
+    print(f"Median Loan Amount: {median_loan_amount:.2f}")
+    print(f"Standard Deviation of Loan Amount: {std_loan_amount:.2f}")
+
+
+
